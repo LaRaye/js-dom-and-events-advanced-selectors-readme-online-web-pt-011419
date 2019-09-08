5644b68ba43b2da750a9a2f67ba76197effd7638
LaRaye Johnson <lnj2105@gmail.com>
2019-09-08 20:57:55 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,11 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  const elements = $('p');
+  return elements;
+}
+
+function lastImageSelector() {
+  const image = $('div img:last');
+  return image;
+}
